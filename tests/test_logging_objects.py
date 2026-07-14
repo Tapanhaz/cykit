f10fe957534beda01dde3ac1cc4d65a9a6990e57
Tapanhaz <tapanhaz@gmail.com>
2026-07-14 22:03:56 +0530

    Update logging object test

--- tests/test_logging_objects.py
+++ tests/test_logging_objects.py
@@ -10,17 +10,29 @@ if sys.platform == "win32":
 
 from cykit.cylogger import Logger, LogLevel
 
-logger = Logger("default", level=LogLevel.DEBUG)
-
 
 def test_log_objects():
+    old_stdout_fd = os.dup(1)
+    old_stderr_fd = os.dup(2)
+
     r, w = os.pipe()
 
-    os.dup2(w, 1)
+    try:
+        os.dup2(w, 1)
+        os.dup2(w, 2)
 
-    logger.info({"a": 1})
+        logger = Logger("default", level=LogLevel.DEBUG)
+        logger.info({"a": 1})
 
-    os.close(w)
+        sys.stdout.flush()
+        sys.stderr.flush()
+    finally:
+        os.dup2(old_stdout_fd, 1)
+        os.dup2(old_stderr_fd, 2)
+        os.close(old_stdout_fd)
+        os.close(old_stderr_fd)
+        os.close(w)
+
     output = os.read(r, 1024).decode()
-
     assert "{'a': 1}" in output
+    os.close(r)
