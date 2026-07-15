192cd177a74f2324c1bd63a0f3fefeda19e6f282
Tapanhaz <tapanhaz@gmail.com>
2026-07-15 12:24:16 +0530

    Add debug script

---
+++ tests/logging_handlers/debug_run.py
@@ -0,0 +1,33 @@
+import sys, os, subprocess
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+sys.path.insert(0, script_dir)
+from conftest import TCPServer, UDPServer, HTTPServer, SMTPServer
+
+servers = [TCPServer(), UDPServer(), HTTPServer(), SMTPServer()]
+for s in servers:
+    s.start()
+
+subprocess.run(
+    [
+        "gdb",
+        "-batch",
+        "-ex",
+        "set pagination off",
+        "-ex",
+        "run",
+        "-ex",
+        "thread apply all bt full",
+        "-ex",
+        "quit",
+        "--args",
+        sys.executable,
+        "-u",
+        "run.py",
+    ],
+    cwd=script_dir,
+)
+
+for s in servers:
+    s.stop()
