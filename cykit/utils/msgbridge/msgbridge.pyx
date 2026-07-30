8e89896ec840ee049d2cff91d9d270f2669cba5f
Tapanhaz <tapanhaz@gmail.com>
2026-07-30 18:23:39 +0530

    Use Python threads in SyncDispatcher and msgspec for CBufferView object encoding

--- cykit/utils/msgbridge/msgbridge.pyx
+++ cykit/utils/msgbridge/msgbridge.pyx
@@ -17,31 +17,30 @@ buffer conversion helpers.
 - Supports fixed-size and variable-size message transport.
 """
 
-from cykit.queue cimport QueueMode, Q_OK
+from cykit.queue cimport QueueMode, Q_OK, BridgeQueue
 from cykit.common cimport (
-    make_thread, 
     Py_buffer,
     PyBUF_SIMPLE,
     PyBuffer_Release,
     PyObject_GetBuffer,
     PyUnicode_AsUTF8AndSize,
-    PyObject_Bytes,
     PyBytes_AS_STRING,
     PyBytes_GET_SIZE,
     PyObject_CheckBuffer,
-    PyObject_Str, 
-    PyErr_Clear, 
-    PyBytes_CheckExact, 
-    PyUnicode_CheckExact,
     PyObject,
     Py_INCREF,
     Py_DECREF,
-    PyBytes_FromStringAndSize,
     memory_order_acquire,
     memory_order_release,
     memory_order_seq_cst
 )
 from libc.errno cimport errno, EPERM, EACCES
+
+from cpython.bytes cimport PyBytes_FromStringAndSize
+
+import warnings
+import msgspec
+import threading
 
 import asyncio
 import sys
@@ -59,12 +58,13 @@ cpdef object setup_socket(bint blocking, int recvbuf):
                 sock.setsockopt(_socket.SOL_SOCKET, 33, recvbuf) #SO_RCVBUFFORCE 
             except OSError as e:
                 if e.errno in (EPERM, EACCES):
-                    print(
+                    warnings.warn(
                         "SO_RCVBUFFORCE needs CAP_NET_ADMIN "
-                        "(sudo or setcap). Falling back to SO_RCVBUF."
+                        "(sudo or setcap). Falling back to SO_RCVBUF.",
+                        RuntimeWarning
                     )
                 else:
-                    print(f"WARNING: SO_RCVBUFFORCE failed ({e}). Falling back.")
+                    warnings.warn(f"SO_RCVBUFFORCE failed ({e}). Falling back.", RuntimeWarning)
 
                 sock.setsockopt(_socket.SOL_SOCKET, _socket.SO_RCVBUF, recvbuf)
         else:
@@ -151,6 +151,7 @@ cdef class AsyncDispatcher:
             sig_notify(&self._bridge)
         return ret
      
+
     async def __reader(self, loop, sock):        
         cdef:
             char* buf
@@ -158,7 +159,9 @@ cdef class AsyncDispatcher:
             unsigned int counter = 0 
 
         while self._running:
+            sig_wait_begin(&self._bridge)
             if self._q.try_pop(&buf, &size) == Q_OK:
+                sig_wait_end(&self._bridge)
                 await self._callback(buf[:size])
 
                 counter = (counter + 1) & 127
@@ -166,7 +169,8 @@ cdef class AsyncDispatcher:
                     await asyncio.sleep(0)
 
             else:
-                await loop.sock_recv(sock, 1) 
+                await loop.sock_recv(sock, 1)
+                sig_wait_end(&self._bridge)
                 
     async def __reader_var(self, loop, sock):        
         cdef:
@@ -175,7 +179,9 @@ cdef class AsyncDispatcher:
             unsigned int counter = 0
 
         while self._running:
+            sig_wait_begin(&self._bridge)
             if self._q.try_pop_var(&buf, &size) == Q_OK:
+                sig_wait_end(&self._bridge)
                 await self._callback(buf[:size])
 
                 counter = (counter + 1) & 127
@@ -183,7 +189,8 @@ cdef class AsyncDispatcher:
                     await asyncio.sleep(0)
 
             else:
-                await loop.sock_recv(sock, 1) 
+                await loop.sock_recv(sock, 1)
+                sig_wait_end(&self._bridge)
 
     def close(self)-> None:  
         self._running = False  
@@ -218,19 +225,6 @@ cdef class AsyncDispatcher:
 
 # region Sync Dispatcher
 
-cdef inline void _sync_try_pop_entry(void* arg) noexcept nogil:
-    (<SyncDispatcher>arg).__try_pop()
-
-cdef inline void _sync_try_pop_var_entry(void* arg) noexcept nogil:
-    (<SyncDispatcher>arg).__try_pop_var()
-
-cdef inline void _sync_pop_entry(void* arg) noexcept nogil:
-    (<SyncDispatcher>arg).__pop()
-
-cdef inline void _sync_pop_var_entry(void* arg) noexcept nogil:
-    (<SyncDispatcher>arg).__pop_var()
-
-
 cdef class SyncDispatcher:
 
     def __cinit__(self):
@@ -258,10 +252,11 @@ cdef class SyncDispatcher:
         self._detach = detach
         self._nonblocking = nonblocking
 
-        self._q = Queue(
-                        slot_size= slot_size,
+        self._thread = None
+
+        self._q = BridgeQueue(
                         capacity= capacity,
-                        mode= QueueMode.SPSC,
+                        slot_size= slot_size,
                         overwrite= overwrite,
                         zerocopy= zerocopy,
                         block_on_full= block_on_full
@@ -299,20 +294,22 @@ cdef class SyncDispatcher:
 
             self._bridge.sock = self._sock.fileno()
 
-
         if self._variable_size:
-            if self._nonblocking:
-                self._thread = make_thread(_sync_try_pop_var_entry, <void*>self)
-            else:
-                self._thread = make_thread(_sync_pop_var_entry, <void*>self)
+            target = (
+                self.__try_pop_var
+                if self._nonblocking
+                else self.__pop_var
+            )
         else:
-            if self._nonblocking:
-                self._thread = make_thread(_sync_try_pop_entry, <void*>self) 
-            else:
-                self._thread = make_thread(_sync_pop_entry, <void*>self) 
-        
-        if self._detach:
-            self._thread.detach()
+            target = (
+                self.__try_pop
+                if self._nonblocking
+                else self.__pop
+            )
+
+        self._thread = threading.Thread(target=target, daemon= self._detach)
+
+        self._thread.start()
 
     cdef inline int __try_push(self, const char* data, size_t size) noexcept nogil:
         cdef int ret = self._q.try_push(data, size)
@@ -332,85 +329,55 @@ cdef class SyncDispatcher:
     cdef inline int __push_var(self, const char* data, size_t size) noexcept nogil:
         return self._q.push_var(data, size)
 
-    cdef void __try_pop(self) noexcept nogil:
+    cdef void __try_pop(self):
         cdef:
             char* buf 
             size_t size
-            PyObject* cb 
-        
-        with gil:
-            cb = <PyObject*>self._callback
-            Py_INCREF(cb)
 
         while self._running.load(memory_order_acquire):
+            sig_wait_begin(&self._bridge)
             if self._q.try_pop(&buf, &size) == Q_OK:
-                with gil:
-                    (<object>cb)(<object>PyBytes_FromStringAndSize(buf, size))
+                sig_wait_end(&self._bridge)
+                self._callback(PyBytes_FromStringAndSize(buf, size))
             else:
-                sig_wait(&self._bridge)
-        
-        with gil:
-            Py_DECREF(cb)
-    
-    cdef void __try_pop_var(self) noexcept nogil:
+                with nogil:
+                    sig_wait(&self._bridge)
+                sig_wait_end(&self._bridge)
+            
+    cdef void __try_pop_var(self):
         cdef:
             char* buf 
             size_t size
-            PyObject* cb 
-        
-        with gil:
-            cb = <PyObject*>self._callback
-            Py_INCREF(cb)
 
         while self._running.load(memory_order_acquire):
+            sig_wait_begin(&self._bridge)
             if self._q.try_pop_var(&buf, &size) == Q_OK:
-                with gil:
-                    (<object>cb)(<object>PyBytes_FromStringAndSize(buf, size))
+                sig_wait_end(&self._bridge)
+                self._callback(PyBytes_FromStringAndSize(buf, size))
             else:
-                sig_wait(&self._bridge)
-        
-        with gil:
-            Py_DECREF(cb)
+                with nogil:
+                    sig_wait(&self._bridge)
+                sig_wait_end(&self._bridge)
     
-    cdef void __pop(self) noexcept nogil:
-        cdef:
-            char*  buf
-            size_t size   
 
-            PyObject* cb      
-
-        with gil:
-            cb = <PyObject*>self._callback
-            Py_INCREF(cb)
-
-        while self._running.load(memory_order_acquire):
-            if self._q.pop_borrow(&buf, &size) == Q_OK:
-                with gil:
-                    (<object>cb)(<object>PyBytes_FromStringAndSize(buf, size))
-
-                self._q.pop_commit()
-
-        with gil:
-            Py_DECREF(cb)            
-
-    cdef void __pop_var(self) noexcept nogil:
+    cdef void __pop(self):
         cdef:
             char*  buf
             size_t size
-            PyObject* cb      
 
-        with gil:
-            cb = <PyObject*>self._callback
-            Py_INCREF(cb)
-            
+        while self._running.load(memory_order_acquire):
+            if self._q.pop(&buf, &size) == Q_OK:
+                self._callback(PyBytes_FromStringAndSize(buf, size))
+        
+
+    cdef void __pop_var(self):
+        cdef:
+            char*  buf
+            size_t size
 
         while self._running.load(memory_order_acquire):
             if self._q.pop_var(&buf, &size) == Q_OK:
-                with gil:
-                    (<object>cb)(<object>PyBytes_FromStringAndSize(buf, size))
-        
-        with gil:
-            Py_DECREF(cb)
+                self._callback(PyBytes_FromStringAndSize(buf, size))
     
     cpdef void close(self):
         self._running.store(0, memory_order_seq_cst)
@@ -421,15 +388,15 @@ cdef class SyncDispatcher:
 
         if self._nonblocking:
             with nogil:
-                sig_notify(&self._bridge)                
+                sig_notify(&self._bridge)        
+
+        if not self._detach:
+            if self._thread:     
+                self._thread.join()
         
         if self._sock is not None:
             self._sock.close()
-            self._sock = None        
-
-        if not self._detach:
-            if self._thread.joinable():
-                self._thread.join()
+            self._sock = None               
 
     def __dealloc__(self):
         self.close()
@@ -437,8 +404,7 @@ cdef class SyncDispatcher:
 # endregion
 
 
-# region CyDispatcher (cython -> cython)
-
+# region CyPipe (cython -> cython)
 
 cdef class CyPipe:
 
@@ -495,7 +461,7 @@ cdef class CyPipe:
 # endregion
 
 
-# region CBuffer
+# region CBufferView
 
 cdef inline int buf_to_cbuf(
         object msg,
@@ -528,66 +494,23 @@ cdef inline int str_to_cbuf(
     return 0
 
 
-#cdef inline int obj_to_cbuf(
-#        object msg,
-#        PyObject** pb,
-#        const char** data,
-#        size_t* size
-#    ) except -1:
-#
-#    pb[0] = PyObject_Bytes(<PyObject*>msg)
-#    if pb[0] == NULL:
-#        return -1
-#
-#    data[0] = PyBytes_AS_STRING(pb[0])
-#    size[0] = PyBytes_GET_SIZE(pb[0])
-#
-#    return 0
-
 cdef inline int obj_to_cbuf(
+        object encoder,
         object msg,
         PyObject** pb,
         const char** data,
         size_t* size
     ) except -1:
-    cdef Py_ssize_t n
+    cdef object encoded
 
-    if PyBytes_CheckExact(<PyObject*>msg):
-        pb[0] = <PyObject*>msg
-        Py_INCREF(pb[0])
-        data[0] = PyBytes_AS_STRING(pb[0])
-        size[0] = <size_t>PyBytes_GET_SIZE(pb[0])
-        return 0
+    encoded = encoder.encode(msg)
 
-    if PyUnicode_CheckExact(<PyObject*>msg):
-        pb[0] = <PyObject*>msg
-        Py_INCREF(pb[0])
-        data[0] = <char*>PyUnicode_AsUTF8AndSize(pb[0], &n)
-        if data[0] == NULL:
-            Py_DECREF(pb[0])
-            pb[0] = NULL
-            return -1
-        size[0] = <size_t>n
-        return 0
-
-    pb[0] = PyObject_Bytes(<PyObject*>msg)
-    if pb[0] != NULL:
-        data[0] = PyBytes_AS_STRING(pb[0])
-        size[0] = <size_t>PyBytes_GET_SIZE(pb[0])
-        return 0
-
-    PyErr_Clear()
-
-    pb[0] = PyObject_Str(<PyObject*>msg)
-
-    data[0] = <char*>PyUnicode_AsUTF8AndSize(pb[0], &n)
-    if data[0] == NULL:
-        Py_DECREF(pb[0])
-        pb[0] = NULL
-        return -1
-
-    size[0] = <size_t>n
+    pb[0] = <PyObject*>encoded
+    Py_INCREF(pb[0])
+    data[0] = PyBytes_AS_STRING(pb[0])
+    size[0] = <size_t>PyBytes_GET_SIZE(pb[0])
     return 0
+
 
 cdef inline int bytes_to_cbuf(
         object msg,
@@ -610,7 +533,9 @@ cdef class CBufferView:
         self._pb = NULL
         self._view.buf = NULL
     
-    def __init__(self, MsgKind msg_kind=MsgKind.MIXED) -> None:       
+    def __init__(self, MsgKind msg_kind=MsgKind.MIXED) -> None:    
+
+        self._msgpack_encoder = msgspec.msgpack.Encoder()   
 
         if msg_kind == MsgKind.BYTES:
             self._load = <cb_load_fn_t>self._load_bytes
@@ -643,7 +568,7 @@ cdef class CBufferView:
             Py_DECREF(self._pb)
             self._pb = NULL
 
-        return obj_to_cbuf(msg, &self._pb, &self.data, &self.size)
+        return obj_to_cbuf(self._msgpack_encoder, msg, &self._pb, &self.data, &self.size)        
 
     cdef inline int _load_mixed(self, object msg) except -1:
         if isinstance(msg, bytes):
@@ -666,132 +591,3 @@ cdef class CBufferView:
 
 
 
-
-## Only for testing purpose ::
-## TODO :: Implement CBufferView and add support for varible size msgs
-
-
-# region Async Queue
-
-cdef class AsyncQueue:        
-    def __init__(
-            self,
-            object loop= None,
-            size_t slot_size= 1024,
-            size_t capacity= 16384,
-            bint overwrite= False,
-            bint zerocopy= False,
-            bint block_on_full= False
-        ):
-
-        self._q = Queue(
-                    slot_size= slot_size,
-                    capacity= capacity,
-                    mode= QueueMode.SPSC,
-                    overwrite= overwrite,
-                    zerocopy= zerocopy,
-                    block_on_full= block_on_full
-                )
-
-        self._lock = asyncio.Lock()
-        self._notify_cond = asyncio.Condition(self._lock)
-
-        if loop is None:
-            try:
-                self._loop = asyncio.get_running_loop()
-            except RuntimeError:
-                raise RuntimeError(
-                    "AsyncQueue must be created inside a running event loop."
-                )
-        else:
-            self._loop = loop
-    
-    cpdef start_dispatcher(self, object callback):
-        self._callback = callback        
-        self._task = self._loop.create_task(self.__dispatch()) 
-
-    async def push_buf(self, object msg):
-        cdef:
-            Py_buffer view
-            const char* data 
-            size_t size
-        
-        view.buf = NULL
-
-        try:
-            if buf_to_cbuf(msg, &view, &data, &size)!= -1:
-                if self._q.try_push(data, size) > 0:
-
-                    async with self._notify_cond:
-                        self._notify_cond.notify()
-        finally:
-            if view.buf != NULL:
-                PyBuffer_Release(&view)
-
-    async def push_obj(self, object msg):
-        cdef:
-            PyObject* pb = NULL
-            const char* data
-            size_t size
-
-        try:
-            if obj_to_cbuf(msg, &pb, &data, &size) == 0:
-                if self._q.try_push(data, size) > 0:
-                    async with self._notify_cond:
-                        self._notify_cond.notify()
-
-        finally:
-            if pb != NULL:
-                Py_DECREF(pb)
-    
-    async def push_str(self, object msg):
-        cdef:
-            const char* data
-            size_t size
-
-        if str_to_cbuf(msg, &data, &size) == 0:
-            if self._q.try_push(data, size) > 0:
-                async with self._notify_cond:
-                    self._notify_cond.notify()
-
-
-    async def pop(self):
-        cdef:
-            char* data 
-            size_t size 
-        
-        while True:
-            if self._q.try_pop(&data, &size) == Q_OK:
-                return data[:size]
-            else:
-                async with self._notify_cond:
-                    await self._notify_cond.wait()
-
-    
-    async def __dispatch(self):
-        cdef:
-            char* data 
-            size_t size 
-
-        while True:
-            if self._q.try_pop(&data, &size) == Q_OK:
-                await self._callback(data[:size])
-            
-            else:
-                async with self._notify_cond:
-                    await self._notify_cond.wait()     
-
-
-    async def shutdown(self, *excinfo):
-        async with self._notify_cond:
-            self._notify_cond.notify_all()
-
-        if self._task is not None:
-            self._task.cancel()
-
-            try:
-                await self._task
-            except asyncio.CancelledError:
-                pass
-
-# endregion
